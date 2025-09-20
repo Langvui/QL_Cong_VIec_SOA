a733da2b6f8c093ad55c74f0b8fb999f46c8d13f
DaiAiTienTon <99729215+DaiAiTienTon@users.noreply.github.com>
2025-09-20 10:21:37 +0700

    Revert "Flight"

--- QL_Cong_Viec/QL_Cong_Viec/Controllers/FlightsController.cs
+++ QL_Cong_Viec/QL_Cong_Viec/Controllers/FlightsController.cs
@@ -1,11 +1,4 @@
-using Humanizer;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.IdentityModel.Tokens;
-using NHibernate.Cache;
-using NHibernate.Mapping;
-using QL_Cong_Viec.ESB.Services;
-using QL_Cong_Viec.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using QL_Cong_Viec.Service;
 
 namespace QL_Cong_Viec.Controllers
@@ -13,70 +6,19 @@ namespace QL_Cong_Viec.Controllers
     public class FlightsController : Controller
     {
         private readonly FlightAggregatorService _aggregator;
-        private readonly HealthCheckService _healthCheckService;
-        private readonly IMemoryCache _cache;
 
-        public FlightsController(
-            FlightAggregatorService aggregator, 
-            HealthCheckService healthCheckService, 
-            IMemoryCache cache)
+
+
+        public FlightsController(FlightAggregatorService aggregator)
         {
             _aggregator = aggregator;
-            _healthCheckService = healthCheckService;
-            _cache = cache;
         }
-
-        [HttpGet("Search")]
-        public async Task<IActionResult> Search(FlightSearchDto flightSearch)
-        {
-            if (string.IsNullOrEmpty(flightSearch.From) || string.IsNullOrEmpty(flightSearch.To))
-                return BadRequest("Thiếu tham số from hoặc to");
-
-            string cacheKey = $"flights_{flightSearch.From}_{flightSearch.To}";
-            if (!_cache.TryGetValue(cacheKey, out List<FlightDto> flights))
-            {
-                flights = await _aggregator.GetFlightsWithExtrasAsync(flightSearch.From, flightSearch.To);
-                _cache.Set(cacheKey, flights, TimeSpan.FromMinutes(5));
-            }
-
-            HttpContext.Session.SetString("lastCacheKey", cacheKey);
-            HttpContext.Session.SetString("from", flightSearch.From);
-            HttpContext.Session.SetString("to", flightSearch.To);
-
-            ViewData["from"] = flightSearch.From;
-            ViewData["to"] = flightSearch.To;
-
-            return View("Index", flights);
-        }
-
-        public IActionResult Details(string id)
-        {
-            var cacheKey = HttpContext.Session.GetString("lastCacheKey");
-
-            if (!string.IsNullOrEmpty(cacheKey) && _cache.TryGetValue(cacheKey, out List<FlightDto>? flights))
-            {
-                var flight = flights?.FirstOrDefault(f => f.FlightNumber == id);
-                if (flight != null)
-                {
-                    ViewData["from"] = flight.DepartureIata;
-                    ViewData["to"] = flight.ArrivalIata;
-                    return View(flight);
-                }
-            }
-
-            return NotFound();
-        }
-
         public async Task<IActionResult> Index()
         {
             var flights = await _aggregator.GetFlightsWithExtrasAsync();
+
             return View(flights);
         }
 
-        public async Task<IActionResult> Health()
-        {
-            var healthChecks = await _healthCheckService.GetAllServiceHealthAsync();
-            return Json(healthChecks);
-        }
     }
 }
